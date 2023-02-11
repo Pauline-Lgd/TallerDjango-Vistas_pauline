78d49f7586385882aa8d9e9bbdbb96e82a20fb1c
Pauline-Lgd <Pauline.011008@gmail.com>
2023-02-11 19:17:59 +0100

    update

--- measurements/logic/measurements_logic.py
+++ measurements/logic/measurements_logic.py
@@ -1,5 +1,5 @@
 from ..models import Measurement
-
+from variables.logic.variables_logic import *
 def get_measurements():
     measurement = Measurement.objects.all()
     return measurement
@@ -10,12 +10,15 @@ def get_measurement(meas_pk):
 
 def update_measurement(meas_pk, new_meas):
     measurement = get_measurement(meas_pk)
-    measurement.name = new_meas["name"]
+    measurement.variable = get_variable(new_meas["variable"])
+    measurement.value = new_meas["value"]
+    measurement.unit = new_meas["unit"]
+    measurement.place = new_meas["place"]
     measurement.save()
     return measurement
 
 def create_measurement(meas):
-    measurement = Measurement(name=meas["name"])
+    measurement = Measurement(variable=get_variable(meas["variable"]), value=meas["value"], unit=meas["unit"], place=meas["place"])
     measurement.save()
     return measurement
 
